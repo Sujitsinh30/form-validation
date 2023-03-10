4f47004ba86efb82087aee37ced407411577dec0
= <=>
2023-03-10 18:42:40 +0530

    form design is completed

---
+++ script.js
@@ -0,0 +1,8 @@
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let b_date = document.getElementById('b_date');
+let ap_time = document.getElementById('ap_time');
+
+
